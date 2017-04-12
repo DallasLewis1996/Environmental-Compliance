32c9701691901b5e9ea1e9ce597f1151fbf149e2
Dallas Lewis <Dallas Lewis>
2017-04-12 12:07:12 -0600

    First init of git repos.

---
+++ Environmental Compliance/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Environmental_Compliance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This console app targets .NET 4.6.2");
+            Console.ReadKey(true);
+        }
+    }
+}
